9b1f96844d9d6ba5c6f7f2098ab79a5bca5e31b4
raul.perez-velazquez@eviden.com <raul.perez-velazquez@eviden.com>
2024-09-18 16:51:36 +0200

    Ejercicios 1 y 2

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import beans.Person;
 
 import com.google.common.collect.ImmutableList;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,14 +20,33 @@ public class Lecture1 {
   @Test
   public void imperativeApproach() throws IOException {
     List<Person> people = MockData.getPeople();
+
     // 1. Find people aged less or equal 18
     // 2. Then change implementation to find first 10 people
+    final int limit = 10;
+    int counter = 0;
 
+    List<Person> youngPeople = Lists.newArrayList();
+    for (Person person : people) {
+      if (person.getAge() <= 18) {
+        youngPeople.add(person);
+        counter++;
+        if (counter == limit) {
+          break;
+        }
+      }
+    }
+    for (Person young : youngPeople) {
+      System.out.println(young);
+    }
   }
 
   @Test
   public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
+    MockData.getPeople().stream()
+        .filter(person -> person.getAge() <= 18)
+        .limit(10)
+        .collect(Collectors.toList())
+        .forEach(System.out::println);
   }
 }
--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -10,20 +10,28 @@ public class Lecture2 {
 
   @Test
   public void range() throws Exception {
-
+    System.out.println("Exclusive:");
+    IntStream.range(0, 10).forEach(System.out::println);
+    System.out.println("Inclusive:");
+    IntStream.rangeClosed(0, 9).forEach(System.out::println);
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
-    List<Person> people = MockData.getPeople();
+    // Complete method bellow: loop through people using IntStream and print the each object.
+    // Think weather you need to use range or range close.
 
+    List<Person> people = MockData.getPeople();
+    IntStream.range(0, people.size()).forEach(index -> System.out.println(people.get(index)));
   }
 
   @Test
   public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
+    // Pintar números pares usando IntStream.iterate
+
+    IntStream.iterate(0, operand -> operand + 1)
+        .filter(value -> value % 2 == 0)
+        .limit(20)
+        .forEach(System.out::println);
   }
 }
